d48244a8238c3574198ded95ff142e194a00673e
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-09-28 13:33:21 +0900

    chore: preload suit variable font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+	import suitVariableWoff2 from '@sun-typeface/suit/fonts/variable/woff2/SUIT-Variable.woff2?url';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<link rel="preload" href={suitVariableWoff2} as="font" type="font/woff2" />
+</svelte:head>
 
 {@render children?.()}

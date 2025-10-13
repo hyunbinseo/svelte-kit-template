1a7ac9dfbec20b8a5649b59f14d7b28e680e9832
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-10-13 18:46:29 +0900

    chore: mre for svelte-kit-14707

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,27 @@
 <script lang="ts">
 	import { FormSchema } from './ids';
-	import { addId, getIds } from './ids.remote';
+	import { addId } from './ids.remote';
 </script>
 
-<main class="p-8">
-	<form {...addId.preflight(FormSchema)} oninput={() => addId.validate()}>
-		<fieldset disabled={!!addId.pending} class="w-fit">
-			<label class="flex flex-col *:w-fit">
-				<span class="font-bold">UUID</span>
-				<input
-					{...addId.fields.uuid.as('text')}
-					required
-					size="36"
-					autocomplete="off"
-					class="font-mono"
-				/>
+<!-- eslint-disable svelte/require-each-key -->
+
+<form {...addId.preflight(FormSchema)}>
+	<fieldset>
+		{#each ['windows', 'mac', 'linux'] as os, index}
+			<label>
+				<!-- windows is not checked -->
+				<input {...addId.fields.operatingSystem.as('radio', os)} defaultChecked={!index} />
+				{os}
 			</label>
-			<!-- eslint-disable-next-line svelte/require-each-key -->
-			{#each addId.fields.uuid.issues() ?? [] as issue}
-				<p>{issue.message}</p>
-			{/each}
-			<div
-				class={[
-					'mt-2 flex justify-between',
-					'*:rounded *:bg-blue-100 *:px-2 *:py-1 *:text-sm *:disabled:bg-gray-200',
-				]}
-			>
-				<button
-					type="button"
-					onclick={() => {
-						addId.fields.uuid.set(self.crypto.randomUUID());
-						addId.validate();
-					}}
-				>
-					Generate
-				</button>
-				<button class="disabled:after:content-['ing']">Submit</button>
-			</div>
-		</fieldset>
-	</form>
-	<ol class="mt-8 list-decimal pl-8 font-mono">
-		{#each await getIds() as id (id)}
-			<li>{id}</li>
 		{/each}
-	</ol>
-</main>
+	</fieldset>
+	<fieldset>
+		{#each ['windows', 'mac', 'linux'] as os, index}
+			<label>
+				<!-- windows is checked -->
+				<input type="radio" name="os" value={os} defaultChecked={!index} />
+				{os}
+			</label>
+		{/each}
+	</fieldset>
+</form>
--- src/routes/ids.ts
+++ src/routes/ids.ts
@@ -1,5 +1,5 @@
-import { object, pipe, string, uuid } from 'valibot';
+import { object, picklist } from 'valibot';
 
 export const FormSchema = object({
-	uuid: pipe(string(), uuid()),
+	operatingSystem: picklist(['windows', 'mac', 'linux']),
 });

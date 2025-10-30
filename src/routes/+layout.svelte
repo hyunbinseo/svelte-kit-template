7d9f7864a5e9c5cd170ddfbc1b46da31a533ce87
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-10-30 14:29:14 +0900

    chore: add noscript, offline alert

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,11 @@
 	import suitVariableWoff2 from '@sun-typeface/suit/fonts/variable/woff2/SUIT-Variable.woff2?url';
 	import { checkBrowserScript } from '.';
 	import '$lib/app.css';
+	import { slide } from 'svelte/transition';
 
 	let { children } = $props();
+
+	let isOnline = $state<boolean>();
 </script>
 
 <svelte:head>
@@ -18,4 +21,23 @@
 	/>
 </svelte:head>
 
+<svelte:window bind:online={isOnline} />
+
+<noscript>자바스크립트를 사용할 수 없습니다.</noscript>
+
+{#if isOnline === false}
+	<p transition:slide>인터넷에 연결되어 있지 않습니다.</p>
+{/if}
+
 {@render children?.()}
+
+<style>
+	noscript,
+	p {
+		display: block;
+		background-color: var(--color-yellow-300);
+		padding: calc(var(--spacing) * 4);
+		font-weight: var(--font-weight-semibold);
+		text-align: center;
+	}
+</style>

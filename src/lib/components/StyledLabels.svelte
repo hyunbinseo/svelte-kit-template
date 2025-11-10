dd5e15871efbe20335a719e1ffadbff7beec5223
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-11-10 12:08:13 +0900

    refactor: label styles as component

---
+++ src/lib/components/StyledLabels.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	const { children }: { children: Snippet } = $props();
+</script>
+
+<div class="contents">
+	{@render children()}
+</div>
+
+<style>
+	div :global label {
+		&:has(> input:is([type='checkbox'], [type='radio']):first-child + span:last-child) {
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+			column-gap: calc(var(--spacing) * 2);
+		}
+		> span:first-child {
+			+ select,
+				+ textarea,
+				/* Reference https://tailwindcss-forms.vercel.app/kitchen-sink.html */
+				+ input:not([type='checkbox'], [type='radio'], [type='range'], [type='color'], [type='file']) {
+				display: block;
+				margin-top: calc(var(--spacing) * 1);
+			}
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import StyledLabels from '$lib/components/StyledLabels.svelte';
 	import { FormSchema } from './ids';
 	import { addId, getIds } from './ids.remote';
 </script>
@@ -6,41 +7,43 @@
 <main>
 	<section class="p-8">
 		<h2 class="text-xl font-bold">Remote Functions</h2>
-		<form {...addId.preflight(FormSchema)} oninput={() => addId.validate()} class="mt-4">
-			<fieldset disabled={!!addId.pending} class="w-fit">
-				<label>
-					<span class="text-sm font-bold after:required-indicator">UUID</span>
-					<input
-						{...addId.fields.uuid.as('text')}
-						required
-						size="36"
-						autocomplete="off"
-						class="font-mono"
-					/>
-				</label>
-				<!-- eslint-disable-next-line svelte/require-each-key -->
-				{#each addId.fields.uuid.issues() ?? [] as issue}
-					<p>{issue.message}</p>
-				{/each}
-				<div
-					class={[
-						'mt-2 flex justify-between',
-						'*:rounded *:bg-blue-100 *:px-2 *:py-1 *:text-sm *:disabled:bg-gray-200',
-					]}
-				>
-					<button
-						type="button"
-						onclick={() => {
-							addId.fields.uuid.set(self.crypto.randomUUID());
-							addId.validate();
-						}}
+		<StyledLabels>
+			<form {...addId.preflight(FormSchema)} oninput={() => addId.validate()} class="mt-4">
+				<fieldset disabled={!!addId.pending} class="w-fit">
+					<label>
+						<span class="text-sm font-bold after:required-indicator">UUID</span>
+						<input
+							{...addId.fields.uuid.as('text')}
+							required
+							size="36"
+							autocomplete="off"
+							class="font-mono"
+						/>
+					</label>
+					<!-- eslint-disable-next-line svelte/require-each-key -->
+					{#each addId.fields.uuid.issues() ?? [] as issue}
+						<p>{issue.message}</p>
+					{/each}
+					<div
+						class={[
+							'mt-2 flex justify-between',
+							'*:rounded *:bg-blue-100 *:px-2 *:py-1 *:text-sm *:disabled:bg-gray-200',
+						]}
 					>
-						Generate
-					</button>
-					<button class="disabled:after:content-['ting']">Submit</button>
-				</div>
-			</fieldset>
-		</form>
+						<button
+							type="button"
+							onclick={() => {
+								addId.fields.uuid.set(self.crypto.randomUUID());
+								addId.validate();
+							}}
+						>
+							Generate
+						</button>
+						<button class="disabled:after:content-['ting']">Submit</button>
+					</div>
+				</fieldset>
+			</form>
+		</StyledLabels>
 		<ol class="mt-8 list-decimal pl-8 font-mono">
 			{#each await getIds() as id (id)}
 				<li>{id}</li>
@@ -50,27 +53,29 @@
 	<hr />
 	<section class="p-8">
 		<h2 class="text-xl font-bold">Form Controls</h2>
-		<form class="mt-4 flex w-fit flex-col gap-y-4">
-			<label>
-				<input type="checkbox" />
-				<span>Checkbox</span>
-			</label>
-			<label>
-				<input type="radio" required />
-				<span class="after:required-indicator">Radio</span>
-			</label>
-			<label>
-				<span>Text</span>
-				<input type="text" placeholder="Placeholder" />
-			</label>
-			<label>
-				<span class="after:required-indicator">Select</span>
-				<select required>
-					{#each { length: 3 }, index}
-						<option>Option {index + 1}</option>
-					{/each}
-				</select>
-			</label>
-		</form>
+		<StyledLabels>
+			<form class="mt-4 flex w-fit flex-col gap-y-4">
+				<label>
+					<input type="checkbox" />
+					<span>Checkbox</span>
+				</label>
+				<label>
+					<input type="radio" required />
+					<span class="after:required-indicator">Radio</span>
+				</label>
+				<label>
+					<span>Text</span>
+					<input type="text" placeholder="Placeholder" />
+				</label>
+				<label>
+					<span class="after:required-indicator">Select</span>
+					<select required>
+						{#each { length: 3 }, index}
+							<option>Option {index + 1}</option>
+						{/each}
+					</select>
+				</label>
+			</form>
+		</StyledLabels>
 	</section>
 </main>

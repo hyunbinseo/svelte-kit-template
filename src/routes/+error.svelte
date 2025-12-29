c0016463b903d4381939badb2350ee679ed4aad1
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-12-29 20:31:50 +0900

    chore: add custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { resolve } from '$app/paths';
+	import { page } from '$app/state';
+</script>
+
+<main class="p-8">
+	<code>HTTP {page.status}</code>
+	<h1 class="text-xl font-bold">
+		{#if page.status === 401}
+			인증 정보가 없습니다.<br />
+			재접속해 주시기 바랍니다.
+		{:else if page.status === 403}
+			접근 권한이 없습니다.<br />
+			재접속해 주시기 바랍니다.
+		{:else if page.status === 404}
+			페이지를 찾을 수 없습니다.
+		{:else if page.error?.message}
+			{page.error.message}
+		{:else}
+			알 수 없는 오류가 발생했습니다.
+		{/if}
+	</h1>
+	<a href={resolve('/')} class="mt-4 inline-block btn btn-primary">처음으로</a>
+</main>

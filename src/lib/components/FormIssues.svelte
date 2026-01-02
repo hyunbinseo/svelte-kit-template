b68c9104d55ab941c1f1773fecb06b9e8fb1e568
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2026-01-02 13:56:10 +0900

    chore: limit form issues list count

--- src/lib/components/FormIssues.svelte
+++ src/lib/components/FormIssues.svelte
@@ -7,11 +7,15 @@
 	import { slide } from 'svelte/transition';
 </script>
 
-{#snippet formIssues(issues: RemoteFormIssue[] | undefined)}
-	<ul class="mt-2 text-sm text-red-600 *:truncate *:not-first:mt-2">
-		<!-- eslint-disable-next-line svelte/require-each-key -->
-		{#each issues as issue}
-			<li transition:slide>{issue.message}</li>
-		{/each}
-	</ul>
+{#snippet formIssues(issues: RemoteFormIssue[] | undefined, maxLength = 1)}
+	{#if issues?.length}
+		<ul class="*:mt-2 *:truncate *:text-sm *:text-red-600">
+			<!-- eslint-disable-next-line svelte/require-each-key -->
+			{#each issues as issue, index}
+				{#if index < maxLength}
+					<li transition:slide|global>{issue.message}</li>
+				{/if}
+			{/each}
+		</ul>
+	{/if}
 {/snippet}

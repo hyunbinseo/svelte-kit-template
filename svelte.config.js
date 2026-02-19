4bd4f103e44002b9883c0c87fa08aa293b403094
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2026-02-19 09:04:03 +0900

    chore: log build output directory

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,11 @@
 import adapter from '@sveltejs/adapter-node';
+import { env } from 'node:process';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		experimental: { remoteFunctions: true },
-		adapter: adapter({ out: `build/${Date.now()}` }),
+		adapter: adapter({ out: env.SVELTE_KIT_NODE_ADAPTER_OUT }),
 	},
 	compilerOptions: {
 		experimental: { async: true },

25b05d6cfc883476ef953cc78c6c87aa2d686a46
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2026-04-30 15:12:46 +0900

    revert: re-add eslint-config-prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
 import svelte from 'eslint-plugin-svelte';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
@@ -12,6 +13,8 @@ export default defineConfig(
 	js.configs.recommended,
 	ts.configs.recommended,
 	svelte.configs.recommended,
+	prettier,
+	svelte.configs.prettier,
 	{
 		languageOptions: { globals: { ...globals.browser, ...globals.node } },
 		rules: { 'no-undef': 'off' },

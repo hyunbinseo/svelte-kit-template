c3af69a1ad6d5ad655c08f70890a98f35a04ec32
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2026-08-05 16:52:30 +0900

    refactor(auth): rename redirect variables

--- src/lib/server/auth/redirect.ts
+++ src/lib/server/auth/redirect.ts
@@ -3,22 +3,22 @@ import type { ResolvedPathname } from '$app/types';
 
 // NOTE Request event's `url` relate to the page the remote function was called from
 
-const NAME = 'returnTo';
+const REDIRECT_PARAM = 'returnTo';
 
 export const createRedirectUrl = (pathname: ResolvedPathname) => {
 	const event = getRequestEvent();
 	const url = new URL(pathname, event.url);
 	url.search = '';
-	url.searchParams.set(NAME, event.url.pathname + event.url.search);
+	url.searchParams.set(REDIRECT_PARAM, event.url.pathname + event.url.search);
 	return url;
 };
 
 export const getRedirectUrl = () => {
 	const event = getRequestEvent();
-	const returnTo = event.url.searchParams.get(NAME);
-	if (!returnTo) return;
+	const destination = event.url.searchParams.get(REDIRECT_PARAM);
+	if (!destination) return;
 
-	const url = new URL(returnTo, event.url);
+	const url = new URL(destination, event.url);
 
 	// e.g. new URL('//evil.com', 'https://example.com')
 	if (url.origin !== event.url.origin) return;
